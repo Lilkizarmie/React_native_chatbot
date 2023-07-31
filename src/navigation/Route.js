7e3fd9cbaab56d182e23f354f8d034127757dac6
Jeffery Kizarmie Adolor <jefferyadolor18@gmail.com>
2023-07-31 11:07:10 +0100

    implemented chat ui and connected to  bard api

--- src/navigation/Route.js
+++ src/navigation/Route.js
@@ -1,12 +1,19 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { View, Text } from 'react-native'
 import React from 'react'
+import HomeScreen from "../screens/HomeScreen";
+import ChatScreen from "../screens/ChatScreen";
 
-const Route = () => {
+const Stack=createNativeStackNavigator();
+
+
+
+export default function Route() {
   return (
-    <View>
-      <Text>Route</Text>
-    </View>
+    <Stack.Navigator screenOptions={{}}>
+        <Stack.Screen name="home" component={HomeScreen} 
+        options={{headerShown:false}} />
+        <Stack.Screen name="chat" component={ChatScreen} />
+    </Stack.Navigator>
   )
 }
-
-export default Route
